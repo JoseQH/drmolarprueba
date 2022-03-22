d7dd4ba44f81f325279f8497cdccfda52b3a7b07
jose quenta <josequentah@gmail.com>
2022-03-22 18:10:56 -0500

    listo para probar

---
+++ sw.js
@@ -0,0 +1,89 @@
+importScripts('js/sw-utils.js');
+
+
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+const APP_SHELL = [
+    '/',
+    'index.html',
+    'favicon.ico',
+    'runtime.655f305bb70458bf.js',
+    'polyfills.04d468660d515c68.js',
+    'main.5164f1ef04444fe9.js',
+    'img/favicon.ico',
+    'styles.07ddccddc52888be.css',
+    'js/sw-utils.js'
+
+];
+
+
+const APP_SHELL_INMUTABLE = [
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', e => {
+
+
+    const cacheStatic = caches.open(STATIC_CACHE).then(cache =>
+        cache.addAll(APP_SHELL));
+
+    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache =>
+        cache.addAll(APP_SHELL_INMUTABLE));
+
+
+
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+
+});
+
+self.addEventListener('activate', e => {
+
+    const respuesta = caches.keys().then(keys => {
+
+        keys.forEach(key => {
+
+            if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
+                return caches.delete(key);
+            }
+
+        });
+
+    });
+
+    e.waitUntil(respuesta);
+
+});
+
+
+
+
+self.addEventListener('fetch', e => {
+
+
+    const respuesta = caches.match(e.request).then(res => {
+
+        if (res) {
+            return res;
+        } else {
+
+            return fetch(e.request).then(newRes => {
+
+                return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+
+            });
+
+        }
+
+    });
+
+
+
+    e.respondWith(respuesta);
+
+});
